c8ba1f920ed4fc85196f6251bb3b201d4dc201b8
altanner <8190834+altanner@users.noreply.github.com>
2020-09-28 11:06:03 +0100

    refactor sept

--- kalman_filter_python/genKFTracks2d.py
+++ kalman_filter_python/genKFTracks2d.py
@@ -1,98 +1,115 @@
-import matplotlib.pyplot as plt
-
-from matplotlib import rcParams
+import time #! unused
+from pprint import pprint #! unused
+import seaborn #! unused
 import matplotlib as mpl
-
-mpl.use("Agg")
-
-import seaborn
-
-import time
-
-from pprint import pprint
-
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
 from scipy.optimize import linear_sum_assignment
-
-plt.style.use(["seaborn-whitegrid", "seaborn-ticks"])
-rcParams["figure.figsize"] = 12, 8
-rcParams["axes.facecolor"] = "FFFFFF"
-rcParams["savefig.facecolor"] = "FFFFFF"
-rcParams["figure.facecolor"] = "FFFFFF"
-
-rcParams["xtick.direction"] = "in"
-rcParams["ytick.direction"] = "in"
-
-rcParams["mathtext.fontset"] = "cm"
-rcParams["mathtext.rm"] = "serif"
-
-rcParams.update({"figure.autolayout": True})
-
 import numpy as np
+
+
+def plot_config():
+
+    #todo refactor into module
+
+    mpl.use("Agg")
+
+    plt.style.use(["seaborn-whitegrid", "seaborn-ticks"])
+    rcParams["figure.figsize"] = 12, 8
+    rcParams["axes.facecolor"] = "FFFFFF"
+    rcParams["savefig.facecolor"] = "FFFFFF"
+    rcParams["figure.facecolor"] = "FFFFFF"
+
+    rcParams["xtick.direction"] = "in"
+    rcParams["ytick.direction"] = "in"
+
+    rcParams["mathtext.fontset"] = "cm"
+    rcParams["mathtext.rm"] = "serif"
+
+    rcParams.update({"figure.autolayout": True})
+
 
 np.random.seed(42)
 
-d = 1.0  # Distance between planes
-sigma = 15e-2  # Resolution of planes
-N = 5  # Number of planes
-z = 0.1  # Thickness of absorber
-x0 = 0.01  # Radiation length of absorber
-theta0 = 15e-3  # Multiple scattering uncertainty (TODO: use formula)
+plane_distance = 1.0  # d = Distance between planes
+sigma = 15e-2         # sigma = Resolution of planes
+plane_count = 5       # N = Number of planes
+z = 0.1               # z = Thickness of absorber #! unused
+x0 = 0.01             # x0 = Radiation length of absorber #! unused
+theta0 = 15e-3        # theta0 = Multiple scattering uncertainty
+#(TODO: use formula) < legacy
 
-initialThetaRange = [-np.arcsin(1 / 5.0), np.arcsin(1 / 5.0)]
-initialPhiRange = [-np.arcsin(1 / 5.0), np.arcsin(1 / 5.0)]
+initial_theta_range = [-np.arcsin(1 / 5.0),
+                       np.arcsin(1 / 5.0)]
 
-xRange = [N * d * np.tan(initialThetaRange[0]), N * d * np.tan(initialThetaRange[1])]
-yRange = [N * d * np.tan(initialPhiRange[0]), N * d * np.tan(initialPhiRange[1])]
+initial_phi_range = initial_theta_range
+#initial_phi_range = [-np.arcsin(1 / 5.0),
+ #                  np.arcsin(1 / 5.0)]
+
+x_range = [plane_count * plane_distance * np.tan(initial_theta_range[0]),
+           plane_count * plane_distance * np.tan(initial_theta_range[1])]
+
+y_range = x_range
+#y_range = [plane_count * plane_distance * np.tan(initial_phi_range[0]),
+ #         plane_count * plane_distance * np.tan(initial_phi_range[1])]
 
 
-def plotHits(N, plotTracks, digiHits, planeRange, name, ax=None):
+def plot_hits(plane_count, plot_tracks, digi_hits, plane_range, name, ax=None):
 
-    for i in range(1, N + 1):
+    """
+    #todo docstring
+    """
+
+    for i in range(1, plane_count + 1):
         ax.plot([i, i], [-3, 3], color="k", linestyle="--", alpha=0.15)
 
-    ax.plot(np.array(range(0, N + 1)), plotTracks.T)
-    ax.plot(np.array(range(1, N + 1)), digiHits.T, "x", color="k")
+    ax.plot(np.array(range(0, plane_count + 1)), plot_tracks.T)
+    ax.plot(np.array(range(1, plane_count + 1)), digi_hits.T, "x", color="k")
 
-    ax.set_ylim(planeRange[0] - 0.1, planeRange[1] + 0.1)
-    ax.set_xlim(-0.25, N + 0.25)
+    ax.set_ylim(plane_range[0] - 0.1, plane_range[1] + 0.1)
+    ax.set_xlim(-0.25, plane_count + 0.25)
 
 
-def plotHits2d(N, plotTracks, digiHits, planeRangeX, planeRangeY, ax=None):
+def plot_hits2d(plane_count, plot_tracks, digi_hits, plane_range_x, plane_range_y, ax=None):
+
+    """
+    #todo docstring
+    """
 
     # Plot this first as a hack to get the track colour consistent between plots
 
-    ax.plot(plotTracks[:, :, 0].T, plotTracks[:, :, 1].T)
+    ax.plot(plot_tracks[:, :, 0].T, plot_tracks[:, :, 1].T)
 
-    for p in range(N):
+    for p in range(plane_count):
 
         # First plane
-        xHits = digiHits[:, p, 0]
-        yHits = digiHits[:, p, 1]
+        x_hits = digi_hits[:, p, 0]
+        y_hits = digi_hits[:, p, 1]
 
-        ax.plot(xHits, yHits, "x")
-        ax.set_xlim(planeRangeX[0] - 0.1, planeRangeX[1] + 0.1)
-        ax.set_ylim(planeRangeY[0] - 0.1, planeRangeY[1] + 0.1)
+        ax.plot(x_hits, y_hits, "x")
+        ax.set_xlim(plane_range_x[0] - 0.1, plane_range_x[1] + 0.1)
+        ax.set_ylim(plane_range_y[0] - 0.1, plane_range_y[1] + 0.1)
 
 
 def dist(h1, h2):
     return np.linalg.norm(h1 - h2)
 
 
-def exchangeTrackHits(recoHits, frac=0.2, prob=0.2):
+def exchange_track_hits(reco_hits, frac=0.2, prob=0.2):
 
-    newHits = recoHits.copy()
+    new_hits = reco_hits.copy()
 
     # Exchange closest frac hits with probability prob
     # Only exchange hits on the same plane
 
     # Want to avoid exchanging hits for same tracks?
 
-    for plane in range(N):
+    for plane in range(plane_count):
 
-        planeHits = newHits[:, plane, :]
+        plane_hits = new_hits[:, plane, :]
 
-        k = int(len(planeHits) * frac)
-        select = int(len(planeHits) * frac * prob)
+        k = int(len(plane_hits) * frac)
+        select = int(len(plane_hits) * frac * prob)
 
         # Do the naive thing first, revisit if it's too slow
 
@@ -100,9 +117,9 @@ def exchangeTrackHits(recoHits, frac=0.2, prob=0.2):
             [
                 [
                     (dist(h1, h2) if not np.all(np.equal(h1, h2)) else np.inf)
-                    for h1 in planeHits
+                    for h1 in plane_hits
                 ]
-                for h2 in planeHits
+                for h2 in plane_hits
             ]
         )
 
@@ -123,83 +140,84 @@ def exchangeTrackHits(recoHits, frac=0.2, prob=0.2):
 
         # Do the exchange
 
-        planeHits[[x[0] for x in s], :], planeHits[[x[1] for x in s], :] = (
-            planeHits[[x[1] for x in s], :],
-            planeHits[[x[0] for x in s], :],
+        plane_hits[[x[0] for x in s], :], plane_hits[[x[1] for x in s], :] = (
+            plane_hits[[x[1] for x in s], :],
+            plane_hits[[x[0] for x in s], :],
         )
 
-    return newHits
+    return new_hits
 
 
-def genTracks(nGen=10, truthOnly=False, plot=False, exchangeHits=False):
+def gen_tracks(n_gen=10, truthOnly=False, plot=False, exchange_hits=False):
 
-    # Absorber lengths add
-    msDists = np.array([i * d for i in range(1, N + 1)])
+    # Absorber lengths add #! unused
+    msDists = np.array([i * plane_distance for i in range(1, plane_count + 1)])
 
     # But resulting MS uncertainties add in quadrature
     # TODO: Correct for the actual path length (more oblique tracks see more material)
-    msErrors = np.array([np.sqrt(i) * theta0 for i in range(1, N + 1)])
+    scatter_errors = np.array([np.sqrt(i) * theta0 for i in range(1, plane_count + 1)])
 
-    xBins = np.arange(xRange[0] - 2 * sigma, xRange[1] + 2 * sigma, sigma)
-    yBins = np.arange(yRange[0] - 2 * sigma, yRange[1] + 2 * sigma, sigma)
+    x_bins = np.arange(x_range[0] - 2 * sigma, x_range[1] + 2 * sigma, sigma)
+    y_bins = np.arange(y_range[0] - 2 * sigma, y_range[1] + 2 * sigma, sigma)
 
-    tanThetas = np.tan(np.random.uniform(*initialThetaRange, nGen))
-    tanPhis = np.tan(np.random.uniform(*initialPhiRange, nGen))
+    tan_thetas = np.tan(np.random.uniform(*initial_theta_range, n_gen))
+    tan_phis = np.tan(np.random.uniform(*initial_phi_range, n_gen))
 
-    # tanThetas = np.tan([0 for i in range(nGen)])
-    xTrueHits = np.outer(tanThetas, d * np.array(range(1, N + 1)))
-    yTrueHits = np.outer(tanPhis, d * np.array(range(1, N + 1)))
+    # tan_thetas = np.tan([0 for i in range(n_gen)])
+    x_true_hits = np.outer(tan_thetas, plane_distance * np.array(range(1, plane_count + 1)))
+    y_true_hits = np.outer(tan_phis, plane_distance * np.array(range(1, plane_count + 1)))
 
-    trueHits = np.stack((xTrueHits, yTrueHits), -1)
+    true_hits = np.stack((x_true_hits, y_true_hits), -1)
 
-    xPlotTracks = np.hstack(
-        (np.zeros((nGen, 1)), xTrueHits)
+    x_plot_tracks = np.hstack(
+        (np.zeros((n_gen, 1)), x_true_hits)
     )  # Project tracks back to origin @ -1
-    yPlotTracks = np.hstack(
-        (np.zeros((nGen, 1)), yTrueHits)
+    y_plot_tracks = np.hstack(
+        (np.zeros((n_gen, 1)), y_true_hits)
     )  # Project tracks back to origin @ -1
 
-    plotTracks = np.stack((xPlotTracks, yPlotTracks), -1)
+    plot_tracks = np.stack((x_plot_tracks, y_plot_tracks), -1)
 
     # Fix me
-    msGauss = np.random.normal(np.zeros(N), msErrors, (nGen, N))
+    msGauss = np.random.normal(np.zeros(plane_count), scatter_errors, (n_gen, plane_count))
 
-    xHits = xTrueHits + msGauss
-    yHits = yTrueHits + msGauss
+    x_hits = x_true_hits + msGauss
+    y_hits = y_true_hits + msGauss
 
-    xHitMap = np.digitize(xHits, xBins)
-    xDigiHits = xBins[xHitMap]
+    x_hit_map = np.digitize(x_hits, x_bins)
+    x_digi_hits = x_bins[x_hit_map]
 
-    yHitMap = np.digitize(yHits, yBins)
-    yDigiHits = yBins[yHitMap]
+    y_hit_map = np.digitize(y_hits, y_bins)
+    y_digi_hits = y_bins[y_hit_map]
 
-    digiHits = np.stack((xDigiHits, yDigiHits), -1)
+    digi_hits = np.stack((x_digi_hits, y_digi_hits), -1)
 
-    if exchangeHits:
-        digiHits = exchangeTrackHits(digiHits, frac=0.35, prob=0.75)
+    if exchange_hits:
+        digi_hits = exchange_track_hits(digi_hits, frac=0.35, prob=0.75)
 
     if plot:
 
         fig = plt.figure(figsize=(8, 8))
 
         ax = fig.add_subplot(2, 2, 1)
-        plotHits(N, xPlotTracks, xDigiHits, xRange, "x", ax=ax)
+        plot_hits(plane_count, x_plot_tracks, x_digi_hits, x_range, "x", ax=ax)
 
         ax = fig.add_subplot(2, 2, 2)
-        plotHits(N, yPlotTracks, yDigiHits, yRange, "y", ax=ax)
+        plot_hits(plane_count, y_plot_tracks, y_digi_hits, y_range, "y", ax=ax)
 
         ax = fig.add_subplot(2, 2, 3)
-        plotHits2d(N, plotTracks, digiHits, xRange, yRange, ax=ax)
+        plot_hits2d(plane_count, plot_tracks, digi_hits, x_range, y_range, ax=ax)
 
         plt.savefig("multi.pdf")
         plt.clf()
 
     if not truthOnly:
-        return digiHits, plotTracks
+        return digi_hits, plot_tracks
     else:
-        return trueHits
+        return true_hits
 
 
 if __name__ == "__main__":
 
-    genTracks(nGen=100, plot=True)
+    plot_config()
+    gen_tracks(n_gen=10, plot=True)
